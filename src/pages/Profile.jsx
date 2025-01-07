72673d964f277408ba4e9e3f47969ceb76f3609e
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2025-01-08 06:21:33 +0700

    Add watchlist and favorites functionality to Profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useStore } from '../store'
 import { signOut } from 'firebase/auth';
@@ -6,7 +6,7 @@ import { auth } from '../config/firebase';
 import PropTypes from 'prop-types';
 
 // service
-import { getRatings } from '../service/UserService';
+import { getWatchlists, getFavorites, getRatings } from '../service/UserService';
 
 // components
 import {
@@ -16,10 +16,12 @@ import {
     Box,
     Tabs,
     Tab,
+    ImageList,
 } from '@mui/material';
 import styles from './Profile.module.css';
 import Header from "../components/Header";
 import VoteAverageCircle from '../components/VoteAverageCircle';
+import MovieItem from '../components/MovieItem';
 
 // tabs
 CustomTabPanel.propTypes = {
@@ -50,34 +52,87 @@ function a11yProps(index) {
 }
 
 const Profile = () => {
-    const user = useStore((state) => state.user);
     const navigate = useNavigate();
-
-    const [ averageRating, setAverageRating ] = useState(0);
-    
     const handleLogout = () => {
         signOut(auth);
         navigate('/login');
     }
 
-    // tabs
+    // user
+    const user = useStore((state) => state.user);
     const [tab, setTab] = useState(0);
+    const [watchlists, setWatchlists] = useState([]);
+    const [favorites, setFavorites] = useState([]);
+    const [ratings, setRatings] = useState([]);
+    const [averageRating, setAverageRating] = useState(0);
+    const [error, setError] = useState(null);
+
+    
+    // tabs watchlists, favorites, ratings
     const handleTabChange = (event, newValue) => {
         setTab(newValue);
     };
 
+    // fetch watchlists, favorites, ratings 
+    const fetchWatchlists = async () => {
+        const res = await getWatchlists({ token: user.accessToken });
+        if (res.statusCode !== 200) {
+            setError(res.message);
+            return;
+        }
+        if (res) {
+            setWatchlists(res.data.watchlists);
+        }
+    };
+    const fetchFavorites = async () => {
+        const res = await getFavorites({ token: user.accessToken });
+        if (res.statusCode !== 200) {
+            setError(res.message);
+            return;
+        }
+        if (res) {
+            setFavorites(res.data.favorites);
+        }
+    };
+    const fetchRatings = async () => {
+        const res = await getRatings({ token: user.accessToken });
+        if (res.statusCode !== 200) {
+            setError(res.message);
+            return;
+        }
+        if (res) {
+            setRatings(res.data.ratings);
+        }
+        return res.data.ratings;
+    };
+
+    // fetch watchlists, favorites, ratings when tab changes
     useEffect(() => {
-        const fetchRatings = async () => {
-            const res = await getRatings({ token: user.accessToken });
-            console.log(res);
-            if (res) {
-                const ratings = res.data.ratings;
-                const totalRating = ratings.reduce((acc, rating) => acc + rating.rating, 0);
-                setAverageRating(totalRating / ratings.length / 10);
-            }
-        };
-        fetchRatings();
-    }, [user])
+        setError(null);
+        if (tab === 0) {
+            fetchWatchlists();
+        } else if (tab === 1) {
+            fetchFavorites();
+        } else if (tab === 2) {
+            fetchRatings();
+        }
+    }, [tab]);
+
+    // fetch ratings on first render to calculate average rating
+    const firstRender = useRef(true);
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            fetchRatings();
+        }
+    }, [firstRender]);
+    // calculate average rating
+    useEffect(() => {
+        if (ratings.length > 0) {
+            const totalRating = ratings.reduce((acc, curr) => acc + curr.rating, 0);
+            setAverageRating(totalRating / ratings.length / 10);
+        }
+    }, [ratings]);
 
     return (
         <div className={styles.container}>
@@ -110,7 +165,7 @@ const Profile = () => {
                                 }}
                             >
                                 <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{user.displayName}</Typography>
-                                <Typography> Thành viên kể từ {new Date(user.metadata.creationTime).toLocaleString('default', { month: 'long', year: 'numeric' })}</Typography>
+                                <Typography> Member since {new Date(user.metadata.creationTime).toLocaleString('default', { month: 'long', year: 'numeric' })}</Typography>
                             </Stack>
                             <Stack
                                 direction="row"
@@ -128,29 +183,57 @@ const Profile = () => {
                                             height: '100%'
                                         }}
                                     >
-                                        Đánh giá trung bình
+                                        Average rating
                                     </Typography>
                                 </Box>
                             </Stack>
                         </Stack>
                     </Stack>
                     <Box sx={{ width: '100%' }}>
+
                         <Box sx={{ borderBottom: 1, borderColor: 'divider', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                             <Tabs value={tab} onChange={handleTabChange} aria-label="basic tabs example">
                                 <Tab label="Watchlist" {...a11yProps(0)} />
-                                <Tab label="Favourites" {...a11yProps(1)} />
+                                <Tab label="Favorites" {...a11yProps(1)} />
                                 <Tab label="Ratings" {...a11yProps(2)} />
                             </Tabs>
                         </Box>
+                        
                         <CustomTabPanel value={tab} index={0}>
-                            Watchlist
+                            <ImageList cols={7} sx={{ justifyContent: 'center', margin: '0' }}>
+                                {watchlists.map((item) => (
+                                    <MovieItem key={item.movie._id} item={item.movie} />
+                                ))}
+                            </ImageList>
                         </CustomTabPanel>
                         <CustomTabPanel value={tab} index={1}>
-                            Favourites
+                            <ImageList cols={7} sx={{ justifyContent: 'center', margin: '0' }}>
+                                {favorites.map((item) => (
+                                    <MovieItem key={item.movie._id} item={item.movie} />
+                                ))}
+                            </ImageList>
                         </CustomTabPanel>
                         <CustomTabPanel value={tab} index={2}>
-                            Ratings
+                            <ImageList cols={7} sx={{ justifyContent: 'center', margin: '0' }}>
+                                {ratings.map((item) => (
+                                    <MovieItem key={item.movie._id} item={item.movie} />
+                                ))}
+                            </ImageList>
                         </CustomTabPanel>
+
+                        {error &&
+                            <Typography
+                                color="error"
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    height: '100%'
+                                }}
+                            >
+                                {error}
+                            </Typography>
+                        }
                     </Box>
                 </div>
             ) : (
--- src/service/UserService.jsx
+++ src/service/UserService.jsx
@@ -9,8 +9,13 @@ export const addFavoriteMovie = async ({token, movieID}) => {
 export const removeFavoriteMovie = async ({token, movieID}) => {
     return makeRequest(HTTP_METHOD.DELETE, `${USER_URL}/favorite/${movieID}`, null, true, token);
 }
+
 export const getFavoriteMovie = async ({token, movieID}) => {
     return makeRequest(HTTP_METHOD.GET, `${USER_URL}/favorite/${movieID}`, null, true, token);
+}
+
+export const getFavorites = async ({ token }) => {
+    return makeRequest(HTTP_METHOD.GET, `${USER_URL}/favorite`, null, true, token);
 }
 
 export const addWatchlistMovie = async ({token, movieID}) => {
@@ -25,16 +30,20 @@ export const getWatchlistMovie = async ({token, movieID}) => {
     return makeRequest(HTTP_METHOD.GET, `${USER_URL}/watchlist/${movieID}`, null, true, token);
 }
 
+export const getWatchlists = async ({ token }) => {
+    return makeRequest(HTTP_METHOD.GET, `${USER_URL}/watchlist`, null, true, token);
+}
+
 export const addRatingMovie = async ({token, movieID, rating}) => {
     return makeRequest(HTTP_METHOD.POST, `${USER_URL}/rating`, {movieID, rating}, true, token);
 }
 
-export const getRatings = async ({ token }) => {
-    return makeRequest(HTTP_METHOD.GET, `${USER_URL}/rating`, null, true, token);
-}
-
 export const getRatingMovie = async ({token, movieID}) => {
     return makeRequest(HTTP_METHOD.GET, `${USER_URL}/rating/${movieID}`, null, true, token);
+}
+
+export const getRatings = async ({ token }) => {
+    return makeRequest(HTTP_METHOD.GET, `${USER_URL}/rating`, null, true, token);
 }
 
 export const addReviewMovie = async ({token, movieID, review}) => {
